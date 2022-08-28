820dd8b534ba90a792b10b6232d32f5f9f68f1ac
Iilya <ilya.sobolev@engym.com>
2022-08-28 20:59:08 +0300

    1

--- InteractionEntity/Derivide classes/PhysicalItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class PhysicalItem : InteractionEntity
-{
-    [Header("Object == Door || Arm etc.")]
-    [SerializeField] private bool _alt = false;
-
-    public override void Interact(Player player) => player.CatchPhysicalItem(_alt);
-}
